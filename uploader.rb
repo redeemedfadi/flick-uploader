a8f3e11e1cc0bbebed80f966d6e25e862b286297
Fadi Eliya <fadi.eliya@digitalscientists.com>
2013-09-02 16:09:55 -0400

    Classifying uploader

--- uploader.rb
+++ uploader.rb
@@ -1,29 +1,45 @@
 require 'flickraw'
 
-FlickRaw.api_key = 'API_KEY'
-FlickRaw.shared_secret = 'SECRET'
+class Uploader
 
-creds = {"fullname"=>nil,
- "oauth_token"=>"TOKEN",
- "oauth_token_secret"=>"SECRET",
- "user_nsid"=>"13899279%40N00",
- "username"=>"redeemedfadi"}
+  attr_accessor :creds, :flickr, :my_photo_titles, :my_sets
 
-flickr.access_token = creds["oauth_token"]
-flickr.access_secret = creds["oauth_token_secret"]
+  def initialize
+    FlickRaw.api_key = '08ca5c200381d9ffbd7411a8e7377dd6'
+    FlickRaw.shared_secret = 'a246519777c0069f'
 
-login = flickr.test.login
+    @flickr = FlickRaw::Flickr.new
 
-my_photo_titles = []
-my_sets = []
-page = 1
+    @creds = {"fullname"=>nil,
+     "oauth_token"=>"72157634468645127-72affb2a33ba09b2",
+     "oauth_token_secret"=>"9124b779f3b63648",
+     "user_nsid"=>"13899279%40N00",
+     "username"=>"redeemedfadi"}
 
-while true do
-  photos = flickr.people.getPhotos user_id: 'me', per_page: 500, page: page
+    @flickr.access_token = creds["oauth_token"]
+    @flickr.access_secret = creds["oauth_token_secret"]
 
-  break if photos.none?
+    @login = flickr.test.login
 
-  my_photo_titles = my_photo_titles | photos.map{|p| p["title"]}
+    @my_photo_titles = []
+    @my_sets = []
+  end
 
-  puts page += 1
+
+  def getPhotos
+
+    page = 1
+
+    while true do
+
+      photos = @flickr.people.getPhotos user_id: 'me', per_page: 500, page: page
+
+      break if photos.none?
+
+      @my_photo_titles = @my_photo_titles | photos.map{|p| p["title"]}
+
+      puts page += 1
+    end
+  end
+
 end
